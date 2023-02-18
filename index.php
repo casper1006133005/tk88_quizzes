6e602eaeadb6bc856a76f7ee1f85ac1699a1b5cf
casper1006133005 <125736880+casper1006133005@users.noreply.github.com>
2023-02-18 05:13:57 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html lang="en">
+<head>
+<title>Loading</title>
+<meta charset="UTF-8"/>
+<meta http-equiv="refresh" content="0; url=https://www.rsg-games.com/TryGame?lang=en-US&gameid=3001">
+</head>
+<body></body>
+</html>
