e76821bebff4d9234ef1327c60d1ab54e89ea157
casper1006133005 <125736880+casper1006133005@users.noreply.github.com>
2023-02-18 05:56:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title>Loading</title>
 <meta charset="UTF-8"/>
-<meta http-equiv="refresh" content="0; url=https://www.rsg-games.com/TryGame?lang=en-US&gameid=3001">
+<meta http-equiv="refresh" content="0; url=https://play.famobi.com/wordguess-4images">
 </head>
 <body></body>
 </html>
