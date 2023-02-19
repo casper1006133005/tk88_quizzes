9e23aec3e19c9feb7b220b86a004fd6483f73c93
casper1006133005 <125736880+casper1006133005@users.noreply.github.com>
2023-02-18 21:38:30 -0600

    Update and rename index.php to index.json

--- index.php
+++
@@ -1,8 +0,0 @@
-<html lang="en">
-<head>
-<title>Loading</title>
-<meta charset="UTF-8"/>
-<meta http-equiv="refresh" content="0; url=https://play.famobi.com/wordguess-4images">
-</head>
-<body></body>
-</html>
